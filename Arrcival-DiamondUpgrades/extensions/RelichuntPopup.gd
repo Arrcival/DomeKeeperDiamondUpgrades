fdf7b495cae349c7408d86e667450fef303f0287
Arrcival <arrcival@gmail.com>
2023-11-04 14:49:12 +0100

    feat: cleanup repo and 1.5.8

--- Arrcival-DiamondUpgrades/extensions/RelichuntPopup.gd
+++ Arrcival-DiamondUpgrades/extensions/RelichuntPopup.gd
@@ -2,6 +2,8 @@ extends "res://stages/loadout/RelichuntPopup.gd"
 
 const CONSTARRC = preload("res://mods-unpacked/Arrcival-DiamondUpgrades/Consts.gd")
 
+const MENU_SELECT_NODE = 'ArrcivalDiamondSelect'
+
 func _ready():	
 	var nodeContainer = HBoxContainer.new()
 	
@@ -11,12 +13,13 @@ func _ready():
 	nodeEdit.align = Button.ALIGN_CENTER
 	nodeContainer.add_child(nodeText)
 	nodeContainer.add_child(nodeEdit)
+	nodeContainer.name = MENU_SELECT_NODE
 	$PanelContainer/MarginContainer/VBoxContainer/HBoxContainer5/ModifiersBox/MarginContainer/Box.add_child(nodeContainer)
 	Style.init(self)
 
 func init():
 	.init()
-	var container = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer5/ModifiersBox/MarginContainer/Box.get_child(3)
+	var container = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer5/ModifiersBox/MarginContainer/Box.get_node(MENU_SELECT_NODE)
 	var optionButton = container.get_child(1)
 	optionButton.clear()
 	optionButton.add_item("Default (" + str(CONSTARRC.DEFAULT_DIAMONDS_PER_LAYER) + ")")
@@ -25,7 +28,7 @@ func init():
 
 func emit_signal(stringValue:String):
 	.emit_signal(stringValue)
-	var container = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer5/ModifiersBox/MarginContainer/Box.get_child(3)
+	var container = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer5/ModifiersBox/MarginContainer/Box.get_node(MENU_SELECT_NODE)
 	var optionButton = container.get_child(1)
 	
 	var diamonds = CONSTARRC.DEFAULT_DIAMONDS_PER_LAYER
